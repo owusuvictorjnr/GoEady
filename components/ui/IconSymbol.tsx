f337144d64da6a077fc834936e12b177b0d65042
owusuvictorjnr <owusuvictor80@gmail.com>
2024-12-12 12:18:24 +0000

    add grid

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -13,6 +13,7 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'grid': 'grid-on',
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
