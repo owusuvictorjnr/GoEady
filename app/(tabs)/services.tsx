339e77af7fa39e89ceff63611ea5ec415f682694
owusuvictorjnr <owusuvictor80@gmail.com>
2024-12-13 11:14:00 +0000

    import wrapper component

--- app/(tabs)/services.tsx
+++ app/(tabs)/services.tsx
@@ -1,12 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text } from "react-native";
+import React from "react";
+import Wrapper from "@/components/Wrapper";
 
 const Services = () => {
   return (
-    <View>
+    <Wrapper>
       <Text>Services</Text>
-    </View>
-  )
-}
+    </Wrapper>
+  );
+};
 
-export default Services
+export default Services;
